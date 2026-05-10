6b1842636e931707d9ae3881c08f2e57c37f5e31
pankajkulsange <85685465+pankajkulsange@users.noreply.github.com>
2026-05-10 18:43:50 +0530

    added login scenario in the test

--- tests/locatorsTest.spec.js
+++ tests/locatorsTest.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test.only("Locators Test Scenarios", async ({ page }) => {
+test("Locators Test Scenarios", async ({ page }) => {
   await page.goto("https://pankaj-kulsange.vercel.app/");
   await page.getByRole("button", { name: "Hire Me" }).first().click();
   console.log("test");
@@ -9,4 +9,19 @@ test.only("Locators Test Scenarios", async ({ page }) => {
   await page.locator("button.px-8").first().waitFor();
   const buttonTitles = await page.locator("button.px-8").allTextContents();
   console.log(buttonTitles);
+});
+
+test.only("@Web Client App login", async ({ page }) => {
+  //js file- Login js, DashboardPage
+  const email = "anshika@gmail.com";
+  const productName = "zara coat 3";
+  const products = page.locator(".card-body");
+  await page.goto("https://rahulshettyacademy.com/client");
+  await page.locator("#userEmail").fill(email);
+  await page.locator("#userPassword").type("Iamking@000");
+  await page.locator("[value='Login']").click();
+  await page.waitForLoadState("networkidle");
+  await page.locator(".card-body b").first().waitFor();
+  const titles = await page.locator(".card-body b").allTextContents();
+  console.log(titles);
 });
