cfd191f4eb0d3bab155052ece210669dd54d8abb
pankajkulsange <85685465+pankajkulsange@users.noreply.github.com>
2026-05-12 02:14:21 +0530

    handling multiple pages

--- tests/UIControls.spec.js
+++ tests/UIControls.spec.js
@@ -6,6 +6,7 @@ test.only("UI Controls Test Case", async ({ page }) => {
   // storing paths in const
   const userName = page.locator("#username");
   const signIn = page.locator("#signInBtn");
+  const documentLink = page.locator("[href*='documents-request']");
   // handling static dropdown
   const dropDown = page.locator("select.form-control");
   await dropDown.selectOption("consult");
@@ -13,9 +14,32 @@ test.only("UI Controls Test Case", async ({ page }) => {
   await page.locator(".radiotextsty").last().click();
   await page.locator("#okayBtn").click();
   console.log(await page.locator(".radiotextsty").last().isChecked());
+  // action performed outside that's why await used outside not inside
   await expect(page.locator(".radiotextsty").last()).toBeChecked();
   await page.locator("#terms").click();
   await expect(page.locator("#terms")).toBeChecked();
   await page.locator("#terms").uncheck();
+  // action performed inside that's why await used inside not outside
   expect(await page.locator("#terms").isChecked()).toBeFalsy();
+  // assertion - validating if have the attribute
+  await expect(documentLink).toHaveAttribute("class", "blinkingText");
+});
+test.only("Child windows handling", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const userName = await page.locator("#username");
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const documentLink = page.locator("[href*='documents-request']");
+  const [newPage] = await Promise.all([
+    context.waitForEvent("page"), // listen for any new page, stages of promise (pending, rejected, fulfilled)
+    documentLink.click(), //newe page is open
+  ]);
+  const text = await newPage.locator(".red").textContent();
+  console.log(text);
+  const arrayText = text.split("@");
+  const domain = arrayText[1].split(" ")[0];
+  console.log(domain);
+  await page.locator("#username").type(domain);
+  await page.pause();
+  console.log(await page.locator("#username").inputValue());
 });
